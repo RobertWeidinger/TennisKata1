88bc5d588fd69b8435b0562874d192afab72b060
RobertWeidinger <robert@familie.weidinger-rr.de>
2013-06-18 23:11:33 +0200

    1. Komischer Absturz ist durch Hinzunahme von org.hamcrest.core_... beseitigt (keine Ahnung, was das macht - ich habe den Eindruck, ich habe JUnit4 "zu manuell" eingefügt; da gibt es wahrscheinlich auch einen standardisierteren Weg.
    2. TestIncreasePoints: Jetzt wird die komplette Spezifikation abgeprüft.
    Ist nicht schön, zu viel Cut-and-Paste. Refactoring-Kandidat.

--- src/de/mr/TennisKata1Test/TestScore.java
+++ src/de/mr/TennisKata1Test/TestScore.java
@@ -11,11 +11,28 @@ public class TestScore {
 	@Test
 	public void testIncreasePoints() {
 		Score score = new Score();
+
+		Assert.assertEquals(0, score.getPointsA());
 		score.increasePointsA();
+		Assert.assertEquals(15, score.getPointsA());
 		score.increasePointsA();
-		score.increasePointsB();
 		Assert.assertEquals(30, score.getPointsA());
+		score.increasePointsA();
+		Assert.assertEquals(40, score.getPointsA());
+		score.increasePointsA();
+		Assert.assertEquals(40, score.getPointsA());
+		score.increasePointsA();
+	
+		Assert.assertEquals(0, score.getPointsB());
+		score.increasePointsB();
 		Assert.assertEquals(15, score.getPointsB());
-		
+		score.increasePointsB();
+		Assert.assertEquals(30, score.getPointsB());
+		score.increasePointsB();
+		Assert.assertEquals(40, score.getPointsB());
+		score.increasePointsB();
+		Assert.assertEquals(40, score.getPointsB());
+		score.increasePointsB();
+
 	}
 }
